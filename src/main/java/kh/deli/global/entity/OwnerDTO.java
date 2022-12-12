42e126974ccf19e53da94ab50c72bf518546c081
hye <hezuc12@gmail.com>
2022-12-12 11:45:24 +0900

    refactor:entity DTO 수정

--- src/main/java/kh/deli/global/entity/OwnerDTO.java
+++ src/main/java/kh/deli/global/entity/OwnerDTO.java
@@ -1,2 +1,17 @@
-package kh.deli.global.entity;public class OwnerDTO {
+package kh.deli.global.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OwnerDTO {
+    private int owner_seq;
+    private int acc_seq;
+    private String owner_name;
+    private String owner_phone;
+    private String owner_num;
+    private String owner_card_img;
 }
